806e92443c847ad111f70c9ba515af1b0bb9113c
Nippe <niklaspettersson92@hotmail.com>
2024-10-08 10:19:10 +0200

    Update ToDoListan.py

--- ToDoListan.py
+++ ToDoListan.py
@@ -1,2 +1,13 @@
-def hej():
-    hej = "hej"
+import tkinter
+
+
+#Skapar fönstret
+root = tkinter.Tk()
+root.title("ToDoListan")
+
+rubrik = tkinter.Label(root, text="Dina uppgifter:\n", font=("Arial", 14))
+rubrik.pack()
+
+entry = tkinter.Entry(root, width=50)
+entry.pack(anchor="nw")
+root.mainloop()
