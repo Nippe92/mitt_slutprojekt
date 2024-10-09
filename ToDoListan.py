3815ff4a0c4f7a24bbd227274fa450a975497a89
Nippe <niklaspettersson92@hotmail.com>
2024-10-09 11:32:45 +0200

    Update ToDoListan.py

--- ToDoListan.py
+++ ToDoListan.py
@@ -2,22 +2,36 @@ from datetime import datetime
 import json
 
 class ToDo:
-    def __init__(self, task_name, status):
+    def __init__(self, task_name, status="undone"):
         self.task_name = task_name
         self.status = status
+        self.time_created = datetime.now() #Sätter en tidstämpel när uppgift skapas
+        self.completion_time = None #sätter tidstämpel när uppgift är klar med None som huvudstatus.
+        self.edit_time = datetime.now() # sätter tidstämpel när uppgift uppdateras.
 
-class Todo_list(ToDo):
+    def done_Tasks(self):
+        self.status = "Completed"
+        self.completion_time = datetime.now()
+
+class Todo_list(ToDo): 
     def list_task(self):
-        self.tasks = []
+        self.tasks = [] #Lägger till uppgifterna.
+        self.j_file = [] #Lägger till uppgifter i json fil
 
-    def add_task(self, task):
+    def add_task(self, task_name):
+        task = ToDo(task_name)
         self.tasks.append(task)
+        for task in self.tasks:
+            self.j_file.append(self.task_name, self.status, self.time_created, self.completion_time)
+        
 
     def edit_task(self):
-        pass
+        with open ("ToDoList.json", "a") as file:
+            file.write()
 
     def show_task(self):
-        pass
+        with open ("ToDoList.json", "r") as file:
+            file.read()
 
     def remove_task(self):
         pass
@@ -26,7 +40,10 @@ class Todo_list(ToDo):
         pass
 
     def clear_all(self):
-        self.tasks.clear()
+            self.tasks.clear()
+            self.j_file.clear()
+            
+
 
 
     
