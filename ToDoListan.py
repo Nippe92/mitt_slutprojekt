90b387fe779ba0639ea1dc41d83c644b6459f35d
Nippe <niklaspettersson92@hotmail.com>
2024-10-09 09:11:54 +0200

    Update ToDoListan.py

--- ToDoListan.py
+++ ToDoListan.py
@@ -1,13 +1,34 @@
-import tkinter
+from datetime import datetime
+import json
+
+class ToDo:
+    def __init__(self, task_name, status):
+        self.task_name = task_name
+        self.status = status
+
+class Todo_list(ToDo):
+    def list_task(self):
+        self.tasks = []
+
+    def add_task(self, task):
+        self.tasks.append(task)
+
+    def edit_task(self):
+        pass
+
+    def show_task(self):
+        pass
+
+    def remove_task(self):
+        pass
+
+    def complete_task(self):
+        pass
+
+    def clear_all(self):
+        self.tasks.clear()
 
 
-#Skapar fönstret
-root = tkinter.Tk()
-root.title("ToDoListan")
+    
 
-rubrik = tkinter.Label(root, text="Dina uppgifter:\n", font=("Arial", 14))
-rubrik.pack()
 
-entry = tkinter.Entry(root, width=50)
-entry.pack(anchor="nw")
-root.mainloop()
